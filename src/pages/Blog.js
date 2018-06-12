246f77b24c4a082e6df49407d9d65019c92f5698
Ixl123 <cofman.jacob@gmx.de>
2018-06-12 17:21:03 +0200

    fix empty page

---
+++ src/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const Blog = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default Blog;
