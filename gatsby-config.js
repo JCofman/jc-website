170ee7f482a26a5c18164f2f79137a7d442569a2
Ixl123 <cofman.jacob@gmx.de>
2019-01-29 00:38:16 +0100

    add serviceWorker

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/assets/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
